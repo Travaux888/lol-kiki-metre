9b5c9b4546298d2df8511e3c7c84f7a82f4be3fb
Travaux888 <valentin.lasserre2@gmail.com>
2026-02-19 11:24:02 +0100

    Ajout du code source et de la configuration

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+    print("--- LoL KiKi Metre ---")
+    print("Statut : Cerveau en place et prêt à scanner.")
+    # On ajoutera la logique Riot ce week-end
+    
+if __name__ == "__main__":
+    main()
